c1364e9dc0745a48d5f69e1444b7a9658b626706
sonaliAy <sonalijaiswals2005@gmail.com>
2025-06-30 18:34:23 +0530

    changes

--- frontend/src/api/announcementApi.js
+++ frontend/src/api/announcementApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/announcements';
+const BASE_URL = 'https://notice-board-ploq.onrender.com/api/announcements';
 
 export const fetchAnnouncements = () => axios.get(BASE_URL);
 export const createAnnouncement = (data) => axios.post(BASE_URL, data);
